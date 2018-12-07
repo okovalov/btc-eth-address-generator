cff79c000b56ba6050acfe8afde47ecabd5e2f81
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2018-12-07 09:44:08 -0500

    Ad body parser to the server file

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ dotenv.config();
 
 import express from 'express'
 import { addressCreator } from './components/address-creator'
+import bodyParser from 'body-parser'
 
 const port = process.argv[2]
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post('/newbtc', (req, res) => {
     addressCreator.generateBTCAddress().then( function (result)  {
